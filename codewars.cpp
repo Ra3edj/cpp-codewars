123f8cd45405f0b48ca7eb37e387eaf512ef1c2b
Ra3ed Saade <108870616+Ra3edj@users.noreply.github.com>
2022-11-11 19:57:37 +0200

    Update codewars.cpp

--- codewars.cpp
+++ codewars.cpp
@@ -6,4 +6,58 @@ string even_or_odd(int number)
 {
    return number % 2 == 0 ? "Even" : "Odd";
 }
+// 8 kyu Sum of positive
+#include <vector>
 
+int positive_sum (const std::vector<int> arr){
+  // Your code here
+  int sum = 0;
+  for(int e : arr){
+    if(e >= 0){
+      sum += e;
+    }else{
+      continue;
+    }
+  }
+  return sum;
+}
+//8 kyu Return Negative
+int makeNegative(int num)
+{
+  return num > 0 ? -num : num;
+}
+//8 kyu Reversed Strings
+#include <string>
+
+using namespace std ; 
+
+string reverseString (string str )
+{
+  // your Code is Here ... enjoy !!!
+  reverse(str.begin() , str.end());
+  return str ;
+}
+
+// 8 kyuConvert boolean values to strings 'Yes' or 'No'.
+#include <string>
+using namespace std;
+string bool_to_word(bool value)
+{
+  return value == true ? "Yes" : "No";
+}
+
+//8 kyu Opposite number
+int opposite(int number) 
+{
+ return -number;
+}
+
+// 8 kyu Convert a Number to a String!
+#include <string>
+using namespace std;
+string number_to_string(int num) {
+  // your code here
+  return to_string(num);
+}
+
+// 
