aea5bdf0b49f621a9734067492d970370709c2a4
Ra3ed Saade <108870616+Ra3edj@users.noreply.github.com>
2022-11-16 21:40:12 +0200

    Update codewar(7kyu).cpp

--- codewar(7kyu).cpp
+++ codewar(7kyu).cpp
@@ -44,5 +44,126 @@ string get_middle(string input)
   return x;
 }
 
-// 
+// 7 kyu You're a square!
+#include <cmath>
+bool is_square(int n)
+{
+  // TODO
+  return (double) sqrt(n) - (int) sqrt(n) == 0;
+}
+
+//7 kyu Exes and Ohs
+using namespace std;
+bool XO(const string& str)
+{
+    int x = 0;
+    int o = 0;
+    // your c
+    for (auto i : str) {
+        if (i == 'x' || i == 'X') {
+            x++;
+        }
+        else if (i == 'o' || i == 'O') {
+            o++;
+        }
+    }
+    return o == x;
+}
+
+// 7 kyu Complementary DNA
+#include <string>
+using namespace std;
+string DNAStrand(const string& dna)
+{
+  //your code here
+  string dna1 = "";
+  for(auto i : dna){
+    switch(i){
+        case 'A': dna1 += 'T';
+        break;
+        case 'T': dna1 += 'A';
+        break;
+        case 'G': dna1 += 'C';
+        break;
+        case 'C': dna1 += 'G';
+        break;
+    }
+  }
+  return dna1;
+}
+
+// 7 kyu Sum of two lowest positive integers
+#include <vector>
+using namespace std;
+long sumTwoSmallestNumbers(vector<int> numbers)
+{
+    sort(numbers.begin() , numbers.end());
+  return numbers[0] + numbers[1]; 
+}
+
+// 7 kyu Beginner Series #3 Sum of Numbers
+int get_sum(int a, int b)
+{
+  //Good luck!
+  int s = 0;
+  if(a == b){
+    return a;
+  }else if(b > a){
+    for(int i = a; i <= b; i++){
+      s += i; 
+    }
+    return s;
+  }else if(a > b){
+    for(int i = b; i <= a; i++){
+      s += i; 
+    }
+    return s;
+  }
+}
+
+// 7 kyu Find the next perfect square!
+#include <cmath>
+long int findNextSquare(long int sq) {
+  // Return the next square if sq if a perfect square, -1 otherwise
+  if((double) sqrt(sq) == (int) sqrt(sq)){
+    return (sqrt(sq) + 1) * (sqrt(sq) + 1);
+  }else{
+  return -1;
+}
+}
+
+// 7 kyu Is this a triangle?
+namespace Triangle
+{
+  bool isTriangle(int a, int b, int c)
+  {
+    return (long) a + b > c && (long) a + c > b && (long) b + c > a;
+  }
+};
+
+// 7 kyu Sum of odd numbers
+long long rowSumOddNumbers(unsigned n){
+  //your code here
+  return (long long) n * n * n;
+}
+
+// 7 kyu Odd or Even?
+#include <string>
+#include <vector>
+using namespace std;
+string odd_or_even(const vector<int> &arr)
+{
+    int s = 0;
+  for(auto i : arr){
+    s += i;
+  }return s % 2 == 0 ? "even" : "odd";
+}
+
+//
+
+
+
+
+
+
 
