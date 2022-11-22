0f5233641029e8d2f1433482b454be7920cbe5b5
Ra3ed Saade <108870616+Ra3edj@users.noreply.github.com>
2022-11-22 18:25:47 +0200

    Update codewars(6kyu).cpp

--- codewars(6kyu).cpp
+++ codewars(6kyu).cpp
@@ -1,1 +1,13 @@
+//6 kyu Multiples of 3 or 5
+int solution(int number) 
+{
+  int sum = 0;
+  for(auto i = 0; i < number; i++){
+    if(i % 3 == 0 || i % 5 == 0){
+       sum += i;
+    }
+  }return sum;
+}
+
 //
+
