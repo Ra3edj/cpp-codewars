dcbd7e39a047aed92046fca4ea434ed2ddecc9d5
Ra3ed Saade <108870616+Ra3edj@users.noreply.github.com>
2022-11-25 20:30:44 +0200

    Update codewars(6kyu).cpp

--- codewars(6kyu).cpp
+++ codewars(6kyu).cpp
@@ -63,7 +63,29 @@ string likes(const vector<string> &names)
     names[0] + ", " +  names[1] + " and " + to_string(names.size() - 2) + " others " + "like this"; // Do your magic!
 }
 
-// 
+// 6 kyu Create Phone Number
+#include <string>
+using namespace std;
+string createPhoneNumber(const int arr [10]){
+  //your code here
+  return "(" + to_string(arr[0]) + to_string(arr[1]) + to_string(arr[2]) + ") " 
+    + to_string(arr[3]) + to_string(arr [4]) + to_string(arr[5]) + "-"
+    + to_string(arr[6]) + to_string(arr[7]) + to_string(arr[8]) + to_string(arr[9]);
+}
+
+// 6 kyu Bit Counting
+unsigned int countBits(unsigned long long n){
+  //your code here
+  int c = 0;
+  while(n > 0){
+    c += n % 2;
+    n = n / 2;
+  }
+  return c;
+}
+
+//
+
 
 
 
